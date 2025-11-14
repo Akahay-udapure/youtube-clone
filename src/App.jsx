e2bc5aacf6095c91a95a8b236c5594ed668d8b3a
Akahay-udapure <akshayudapure5@gmail.com>
2025-11-14 08:36:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+    return (
+        <>
+            <div className=" text-xl text-pink-600 text-center py-50">
+                Hello Wolrd
+            </div>
+        </>
+    );
+}
+
+export default App;
